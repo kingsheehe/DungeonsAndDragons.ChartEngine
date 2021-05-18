84b00331b20b185c03fb75d87504e2455230e52c
kingsheehe <84419446+kingsheehe@users.noreply.github.com>
2021-05-18 17:29:13 -0400

    Add files via upload

---
+++ DungeonsAndDragons.ChartEngine/DungeonsAndDragons.ChartEngine/Charts/GetCharts.cs
@@ -0,0 +1,69 @@
+﻿using DungeonsAndDragons.ChartEngine.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDragons.ChartEngine.Charts
+{
+    public class GetCharts
+    {
+        public Dictionary<MonsterType, List<Treasure.MonitaryTreasure>> MonetaryTreasure =
+            new Dictionary<MonsterType, List<Treasure.MonitaryTreasure>>();
+
+        public GetCharts()
+        {
+
+
+            //string monstertype = "A";
+
+
+        }
+
+
+        #region Priviate Methods
+
+        /// <summary>
+        /// Assignment 1)Fill in treaure chart. 2)Write description.
+        /// </summary>
+        public void GetMonetaryChart()
+        {
+            MonetaryTreasure.Add(MonsterType.A, PopulateMonitaryChart());
+
+                           
+        }
+
+        public List<Treasure.MonitaryTreasure> PopulateMonitaryChart()
+        { 
+            //todo   Finish treasure rewards for A 
+            var TreasureRewards = new List<Treasure.MonitaryTreasure>();
+            TreasureRewards.Add(new Treasure.MonitaryTreasure("Copper", Dice.D6, 1000, .25));
+            TreasureRewards.Add(new Treasure.MonitaryTreasure("Silver", Dice.D6, 1000, .30));
+            TreasureRewards.Add(new Treasure.MonitaryTreasure("Electrum", Dice.D4, 1000, .20));
+            TreasureRewards.Add(new Treasure.MonitaryTreasure("Gold", Dice.D6, 1000, .35));//todo  the dice amount is 2d6 not sure how to fix that
+            TreasureRewards.Add(new Treasure.MonitaryTreasure("Platinum", Dice.D2, 1000, .25));
+
+            return TreasureRewards;
+        }
+
+        private void GetGemTreasureChart()
+        {
+
+        }
+
+
+        private void GetJewelryTreasureChart()
+        {
+
+        }
+
+        private void GetMagicItemTreasureChart()
+        {
+
+        }
+
+        #endregion Private Methods
+
+    }
+}
---
+++ DungeonsAndDragons.ChartEngine/DungeonsAndDragons.ChartEngine/Charts/Treasure/JewelryTreasure.cs
@@ -0,0 +1,28 @@
+﻿using DungeonsAndDragons.ChartEngine.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDragons.ChartEngine.Charts.Treasure
+{
+    public class JewelryTreasure
+    {
+
+        /// <summary>
+        /// Name of the treasure.
+        /// </summary>
+        string TreasureName { get; set; }
+
+        /// <summary>
+        /// Type of dice.
+        /// </summary>
+        Dice Dice { get; set; }
+
+        /// <summary>
+        /// Value of jewelry
+        /// </summary>
+
+    }
+}
